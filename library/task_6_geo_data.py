57cb7b07478458df3cb197caa9489e477c3f8326
Tulagunsmith <tulagunsmith@gmail.com>
2022-05-29 12:44:09 +0300

    some changes made

--- library/task_6_geo_data.py
+++ library/task_6_geo_data.py
@@ -6,10 +6,15 @@ ids = {'user1': [213, 213, 213, 15, 213],
        'user2': [54, 54, 119, 119, 119],
        'user3': [213, 98, 98, 35]}
 
-data = []
+data = set()
+
+# for user, num in ids.items():
+#     for i in range(len(num)):
+#         if num[i] not in data:
+#             data.append(num[i])
+
 
 for user, num in ids.items():
     for i in range(len(num)):
-        if num[i] not in data:
-            data.append(num[i])
-print(data)
+        data.add(num[i])
+print(list(data))
